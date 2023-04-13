ac932e89e2808a55b735f466a89fd7be8689bd8c
fairyfade <77501073+fairyfade@users.noreply.github.com>
2023-04-12 22:05:12 -0400

    CompanionPlantingPage
    added a color scheme for the companion plant page.  Made an empty composting page

---
+++ Flora/api/controllers/navbarController.js
@@ -0,0 +1,8 @@
+module.exports = {
+    companion_planting:function(req, res){
+        res.view('companionPlant');
+    },
+    composting:function(req, res){
+        res.view('composting');
+    }
+}
