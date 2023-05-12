6953011e70848a95a82fa42b5ffda233eb735dc2
Sami Cemek <ahmet.cemek24@ncf.edu>
2023-05-12 04:32:19 -0400

    Web scraped 25 plants and formatted to a JSON file
    Added 25 pictures for the plant's scraped
    Set up baseline data for the plants table

--- Flora/api/webScraping/plantScraper.js
+++ Flora/api/webScraping/plantScraper.js
@@ -6,63 +6,121 @@
  */
 
 const puppeteer = require("puppeteer");
+const fs = require("fs");
 
 (async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-        defaultViewport: false,
-        userDataDir: "./tmp",
-    });
+    const browser = await puppeteer.launch();
 
-    const page = await browser.newPage();
-    await page.goto("https://www.bhg.com/gardening/plant-dictionary/bulb/allium/");
-
-
-    const plantName = await page.waitForSelector('#mntl-sc-block_1-0-6 > table', { timeout: 10000 });
-
-    let i = 0;
-    let plants = [];
-
-    (async () => {
-        const browser = await puppeteer.launch();
-
-        const urls = [
+    await (async () => {
+        const urls = [ // 25 plants
             "https://www.bhg.com/gardening/plant-dictionary/bulb/allium/",
-            "https://www.bhg.com/gardening/plant-dictionary/vegetable/arugula/"
-            
+            "https://www.bhg.com/gardening/plant-dictionary/bulb/triumph-tulips/",
+            "https://www.bhg.com/gardening/plant-dictionary/bulb/society-garlic/",
+            "https://www.bhg.com/gardening/plant-dictionary/bulb/anemone-bulb/",
+            "https://www.bhg.com/gardening/plant-dictionary/bulb/gloriosa-lily/",
+            "https://www.bhg.com/gardening/plant-dictionary/perennial/bee-balm/",
+            "https://www.bhg.com/gardening/plant-dictionary/perennial/hollyhock/",
+            "https://www.bhg.com/gardening/plant-dictionary/perennial/perennial-geranium/",
+            "https://www.bhg.com/gardening/plant-dictionary/perennial/spurge/",
+            "https://www.bhg.com/gardening/plant-dictionary/bulb/oxalis/",
+            "https://www.bhg.com/gardening/plant-dictionary/shrub/oleander/",
+            "https://www.bhg.com/gardening/plant-dictionary/annual/wallflower/",
+            "https://www.bhg.com/gardening/plant-dictionary/shrub/privet/",
+            "https://www.bhg.com/gardening/plant-dictionary/herb/sage/",
+            "https://www.bhg.com/gardening/plant-dictionary/perennial/sedum/",
+            "https://www.bhg.com/gardening/plant-dictionary/perennial/thrift/",
+            "https://www.bhg.com/gardening/plant-dictionary/vine/trumpet-vine/",
+            "https://www.bhg.com/gardening/plant-dictionary/perennial/violet/",
+            "https://www.bhg.com/gardening/plant-dictionary/perennial/yucca/",
+            "https://www.bhg.com/gardening/plant-dictionary/shrub/yellow-bells/",
+            "https://www.bhg.com/gardening/plant-dictionary/perennial/yarrow/",
+            "https://www.bhg.com/gardening/plant-dictionary/perennial/veronica/",
+            "https://www.bhg.com/gardening/plant-dictionary/shrub/viburnum/",
+            "https://www.bhg.com/gardening/plant-dictionary/annual/verbena/",
+            "https://www.bhg.com/gardening/plant-dictionary/vine/mandevilla/"
+
         ];
+        console.log("Numeber of plants will scraped: " + urls.length);
 
-        for (const url of urls) {
+        // if the file doesn't exists, add the opening bracket
+        if (!fs.existsSync("plantInfoScraped.json")) {
+            // add the opening bracket to the json file
+            try {
+                //fs.writeFileSync(
+                fs.appendFileSync(
+                    "plantInfoScraped.json",
+                    "[");
+            } catch (error) {
+                console.log(error);
+            }
+        }
 
+        for (let i = 0; i < urls.length; i++) {
+            const url = urls[i];
             const page = await browser.newPage();
             await page.goto(url, { waitUntil: "domcontentloaded" });
 
-            //let plantFeatures = "Null";
-            // try{
-            //     plantFeatures = await page.evaluate(
-            //         (el) => el.querySelector("tr > td.mntl-sc-block-profile__value").textContent, url
-            //     );
-            //     console.log(plantFeatures);
-            // }catch(error){}
+            let plantFeatures = "Null";
 
-            const plantFeatures = await page.$$eval(
+            // Scrape the data from the selector specified
+            plantFeatures = await page.$$eval(
                 "tr > td.mntl-sc-block-profile__value",
                 els => els.map(el => el.textContent)
             );
-            console.log(plantFeatures);
 
-            if (plantFeatures !== "Null") {
-                plants.push({ plantFeatures });
+            console.log(handleArr(plantFeatures));
+
+            // if the last item in the array, don't add a comma
+            if (i === urls.length - 1) {
+                try {
+                    fs.appendFileSync(
+                        "plantInfoScraped.json",
+                        JSON.stringify(handleArr(plantFeatures)));
+                } catch (error) {
+                    console.log(error);
+                }
             }
+            else { //else add a comma
+                try {
+                    fs.appendFileSync(
+                        "plantInfoScraped.json",
+                        JSON.stringify(handleArr(plantFeatures)) + ",");
+                } catch (error) {
+                    console.log(error);
+                }
 
-            await page.close();
-
+                await page.close();
+            }
         }
+
     })()
         .catch(err => console.error(err))
         .finally(() => browser?.close());
 
-    //console.log(plants);
+    // Future work: check if the last line on the file is a bracket if so don't add a bracket again
+    // add the closing bracket to the json file
+        try {
+            fs.appendFileSync(
+                "plantInfoScraped.json",
+                "]");
+        } catch (error) {
+            console.log(error);
+        }
+
     await browser.close();
 
 })();
+
+
+function handleArr(arr) {
+    const labels = ["GENUS NAMES", "COMMON NAME", "PLANT TYPE", "LIGHT", "HEIGHT", "WIDTH", "FLOWER COLOR", "FOLIAGE COLOR", "SEASON FEATURES", "SPECIAL FEATURES", "ZONES", "PROPAGATION", "PROBLEM SOLVERS"]
+    const returnObj = {}
+
+    for (let i = 0; i < arr.length; i++) {
+        const label = labels[i]
+        const value = arr[i]
+        returnObj[label] = value
+    }
+
+    return returnObj
+}
